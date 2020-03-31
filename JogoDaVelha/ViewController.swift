f90614bbc2b061c7e9eab9531bf52edc913a1296
richard <richardp.leandro@gmail.com>
2020-03-31 08:16:10 -0300

    Initial Commit

---
+++ JogoDaVelha/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JogoDaVelha
+//
+//  Created by Richard Leandro on 31/03/20.
+//  Copyright © 2020 Richard Leandro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
